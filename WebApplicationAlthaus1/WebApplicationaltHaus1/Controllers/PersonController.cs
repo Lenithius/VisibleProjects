181e6c2390586ca6a80e68aadd967182a9fd286d
Justin <Justincstace@gmail.com>
2025-06-04 12:23:25 +0100

    Basic WebApp with horrible CSS

---
+++ WebApplicationAlthaus1/WebApplicationaltHaus1/Controllers/PersonController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationaltHaus1.Models;
+using System;
+namespace WebApplicationaltHaus1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private static List<Person> people = new List<Person>();
+
+        [HttpGet]
+        public IActionResult GetAllPeople()
+        {
+            return Ok(people);
+        }
+        [HttpPost]
+        public IActionResult AddPerson([FromBody] Person person)
+        {
+            if (person == null)
+            {
+                return BadRequest("Person data is invalid");
+            }
+            people.Add(person);
+            return CreatedAtAction(nameof(GetAllPeople), new { id = people.Count - 1 }, person);
+
+
+        }
+
+    }
+}
---
+++ WebApplicationAlthaus1/WebApplicationaltHaus1/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿
+namespace WebApplicationaltHaus1.Models
+{
+   
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+    }
+}
